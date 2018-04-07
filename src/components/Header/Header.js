44c16a8dfe97109d8d41f49415898bd0ab5741b2
Gena Riedler <genariedler@Genas-MacBook-Air.local>
2018-04-07 11:43:19 -0700

    header fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import img from "../images/landofooo.jpg"
 
 // const Header = ({ children }) => (
 //   <div className="jumbotron">
@@ -10,6 +11,7 @@ const Header = props => (
     <div className="jumbotron">
         <h1>Adventure Time!</h1>
         <h2>Click on the characters no more than once to win</h2>
+        <img src={img} alt="Adventure Time!" />
     </div>
 )
 
